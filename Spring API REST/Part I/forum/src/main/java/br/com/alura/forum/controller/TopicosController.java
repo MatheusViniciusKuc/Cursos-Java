aca2b39b6f77b4137d4229d7fca86705034f83e6
MatheusViniciusKuc <matheus.v.kuc@gmail.com>
2022-07-20 15:17:31 -0300

    Spring Data

--- Spring API REST/Part I/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Spring API REST/Part I/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,25 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
-import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista() {
-        Topico topico = new Topico("Duvida", "Duúvido com Spring", new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico, topico, topico));
+    public List<TopicoDto> lista(String nomeCurso) {
+        if (nomeCurso != null) {
+            return TopicoDto.converter(topicoRepository.findByCursoNome(nomeCurso));
+        }
+        return TopicoDto.converter(topicoRepository.findAll());
     }
 }
